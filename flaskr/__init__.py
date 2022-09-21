69fd0301b7e119daec193695f647a5c373371f73
arushanovanz <nadin.spb@gmail.com>
2022-09-21 19:28:25 +0300

    added  init_db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,6 +27,9 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello to the new app for quick restaurant search!'
+
+    from . import db
+    db.init_app(app)
 
     return app
